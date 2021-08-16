d3dcf7c7931af28f65ca04d9fb74cbaceecdda9e
Emily N Mayeski <enmayeski@gmail.com>
2021-08-15 23:47:06 -0400

    adjustments to models that make the seeds work, as well as updates to the connection and adding the env file to test db connections

--- models/Product.js
+++ models/Product.js
@@ -28,9 +28,10 @@ Product.init(
     },
     stock: {
       type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id'
+      allowNull: false,
+      defaultValue: 10,      
+      validate: {
+        isNumeric: true
       }
     }
   },
--- models/index.js
+++ models/index.js
@@ -20,7 +20,7 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'tagged_products',
-  foreignKey: 'tag_id'
+  foreignKey: 'product_id'
 });
 
 
@@ -28,7 +28,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'product_tags',
-  foreignKey: 'product_id'
+  foreignKey: 'tag_id'
 });
 
 
